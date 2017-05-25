53ba8c4a836ef08501072260b675ab4c612f94c5
DDGAxel <dragongaidensword@gmail.com>
2017-05-26 01:17:28 +0300

    Add files via upload

---
+++ WADV2/Book_store/includes/menu.inc.php
@@ -0,0 +1,25 @@
+<ul>
+			<li class="current_page_item"><a href="index.php">Home</a></li>
+			<!--<li><a href="register.php">Register</a></li>-->
+			<?php 
+					if(isset($_SESSION['status']))
+					{
+						
+						echo '<li><a href="logout.php">Logout</a></li>';
+					}
+					else
+					{
+						echo '<li><a href="register.php">Register</a></li>';
+					}
+			?>
+			<li><a href="viewcart.php">View Cart</a></li>
+</ul>
+
+			<div id="search" align="right">
+				<form method="GET" action="search_result.php">
+					<fieldset>
+					<input type="text" id="s" name="s" value="" />
+					<input type="submit" id="x" value="Search" />
+					</fieldset>
+				</form>
+			</div>
